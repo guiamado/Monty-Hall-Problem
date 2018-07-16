5dbf9b59edf3d34f717fb37b4ae55c7b33968004
Guilherme Silva <guiamado@localhost.localdomain>
2018-07-16 01:02:59 -0300

    [ADD] Adicionando Projeto Vue e configurando o componente

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Monty Hall Problem</h1>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
+* {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+body {
+    color: #FFF;
+    background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+#app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import App from './App'
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+    render: h=> h(App)
+    // Pode ser feito igualmente abaixo
+    // render(createElement){
+    //    return createElement(App)
+    // }
+}).$mount("#app")
